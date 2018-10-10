cd00073f57b0da6d81d6e37000286fea7cf5a243
Голубых Денис <golubykhda@altarix.ru>
2018-10-10 12:35:26 +0400

    refactor + truncated content

--- src/DiscordHandler.php
+++ src/DiscordHandler.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace DiscordHandler;
 
 use \Monolog\Logger;
@@ -6,46 +7,54 @@ use \Monolog\Handler\AbstractProcessingHandler;
 
 class DiscordHandler extends AbstractProcessingHandler
 {
-	private $initialized = false;
-	private $guzzle;
+    /** @var \GuzzleHttp\Client */
+    protected $guzzle;
 
-	private $name;
-	private $subname;
+    protected $name;
+    protected $subName;
 
-	private $webhooks;
-	private $statement;
+    /** @var array */
+    protected $webHooks;
 
-	/**
-	 * MonologDiscordHandler constructor.
-	 * @param \GuzzleHttp\Client $guzzle
-	 * @param bool $webhooks
-	 * @param int $level
-	 * @param bool $bubble
-	 */
-	public function __construct($webhooks, $name, $subname = '', $level = Logger::DEBUG, $bubble = true)
-	{
-		$this->name = $name;
-		$this->subname = $subname;
-		$this->guzzle = new \GuzzleHttp\Client();
-		$this->webhooks = $webhooks;
-		parent::__construct($level, $bubble);
-	}
+    /**
+     * DiscordHandler constructor.
+     *
+     * @param array|string $webHooks
+     * @param string $name
+     * @param string $subName
+     * @param int $level
+     * @param bool $bubble
+     */
+    public function __construct($webHooks, $name, $subName = '', $level = Logger::DEBUG, $bubble = true)
+    {
+        $this->name = $name;
+        $this->subName = $subName;
+        $this->guzzle = new \GuzzleHttp\Client();
+        $this->webHooks = (array)$webHooks;
+        parent::__construct($level, $bubble);
+    }
 
-	/**
-	 * @param array $record
-	 */
-	protected function write(array $record)
-	{
-		$content = '[' . $record['datetime']->format('Y-m-d H:i:s') . '] ' . $this->name . '.' . $this->subname . '.' . $record['level_name'] . ': ' . $record['message'];
+    /**
+     * @param array $record
+     *
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    protected function write(array $record)
+    {
+        $content = '[**' . $record['datetime']->format('Y-m-d H:i:s') . ']** ' . $this->name . '.' . $this->subName . '.__' . $record['level_name'] . '__: ' . $record['message'];
 
-		$i = 0;
-		while ($i < count($this->webhooks)){
-			$req = $this->guzzle->request('POST', $this->webhooks[$i], [
-				'form_params' => [
-					'content' => $content
-				]
-			]);
-			$i++;
-		}
-	}
+        if (strlen($content) > 2000) {
+            $truncated = strlen($content) - 1950;
+            $content = substr($content, 0, 1950) . " [...truncated $truncated characters]";
+        }
+        foreach ($this->webHooks as $webHook) {
+            $this->guzzle->request(
+                'POST', $webHook, [
+                    'form_params' => [
+                        'content' => $content
+                    ]
+                ]
+            );
+        }
+    }
 }
