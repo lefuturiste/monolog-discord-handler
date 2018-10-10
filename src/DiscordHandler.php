ca48855bc4706f6e3a0d5399ba5d27c74759f377
Голубых Денис <golubykhda@altarix.ru>
2018-10-10 15:19:14 +0400

    add multi message

--- src/DiscordHandler.php
+++ src/DiscordHandler.php
@@ -13,48 +13,70 @@ class DiscordHandler extends AbstractProcessingHandler
     protected $name;
     protected $subName;
 
+    /**
+     * Флаг мульти-сообщений (Если true, то при превышении максимальной длины одного сообщения, будет отправлено несколько)
+     * @var bool
+     */
+    protected $multiMsg;
+
     /** @var array */
     protected $webHooks;
 
     /**
      * DiscordHandler constructor.
      *
-     * @param array|string $webHooks
-     * @param string $name
+     * @param $webHooks
+     * @param $name
+     * @param bool $multiMsg
      * @param string $subName
      * @param int $level
      * @param bool $bubble
      */
-    public function __construct($webHooks, $name, $subName = '', $level = Logger::DEBUG, $bubble = true)
+    public function __construct($webHooks, $name, $multiMsg = false, $subName = '', $level = Logger::DEBUG, $bubble = true)
     {
         $this->name = $name;
         $this->subName = $subName;
         $this->guzzle = new \GuzzleHttp\Client();
         $this->webHooks = (array)$webHooks;
+        $this->multiMsg = $multiMsg;
         parent::__construct($level, $bubble);
     }
 
     /**
      * @param array $record
-     *
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
     protected function write(array $record)
     {
         $content = '[**' . $record['datetime']->format('Y-m-d H:i:s') . ']** ' . $this->name . '.' . $this->subName . '.__' . $record['level_name'] . '__: ' . $record['message'];
 
-        if (strlen($content) > 2000) {
-            $truncated = strlen($content) - 1950;
-            $content = substr($content, 0, 1950) . " [...truncated $truncated characters]";
-        }
         foreach ($this->webHooks as $webHook) {
-            $this->guzzle->request(
-                'POST', $webHook, [
-                    'form_params' => [
-                        'content' => $content
-                    ]
-                ]
-            );
+            if ($this->multiMsg) {
+                foreach (str_split($content, 2000) as $frame) {
+                    $this->send($webHook, $frame);
+                }
+            } else {
+                if (strlen($content) > 2000) {
+                    $truncated = strlen($content) - 1950;
+                    $content = substr($content, 0, 1950) . " [...truncated $truncated characters]";
+                }
+                $this->send($webHook, $content);
+            }
         }
+    }
+
+    /**
+     * @param $webHook
+     * @param $content
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    protected function send($webHook, $content){
+        $this->guzzle->request(
+            'POST', $webHook, [
+                'form_params' => [
+                    'content' => $content
+                ]
+            ]
+        );
     }
 }
