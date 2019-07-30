d88d5a03f92e747e46a8d7473d0f75041f80ba91
Алексей <alexgivi@nextmail.ru>
2019-07-31 01:04:08 +0300

    add flexible config & refactor (#3)
    - add config manager class
    - customize date time format
    - customize message format with `{}` templates
      - datetime
      - name
      - subName
      - levelName
      - message
    - max length on messages to fit into discord requirement of 2000 characters max
    - add support for multi messages if the message doesn't fit in one message of 2000 characters

--- README.md
+++ README.md
@@ -31,9 +31,7 @@ require 'vendor/autoload.php';
 
 $log = new Monolog\Logger('your name');
 
-$log->pushHandler(new DiscordHandler\DiscordHandler([
-'Discord Webhook url'
-], 'name', 'subname', 'DEBUG'));
+$log->pushHandler(new DiscordHandler\DiscordHandler('Discord Webhook url', 'name', 'subname', 'DEBUG'));
 
 ```
 
@@ -50,5 +48,31 @@ $log->pushHandler(new DiscordHandler\DiscordHandler([
 'Discord Webhook url 1',
 'Discord Webhook url 2',
 ], 'name', 'subname', 'DEBUG'));
+
+```
+
+### Use configuration
+ 
+```php
+<?php
+require 'vendor/autoload.php';
+
+$log = new Monolog\Logger('your name');
+
+$handler = new DiscordHandler\DiscordHandler('Webhook url','name', 'subname', 'DEBUG');
+
+$handler->getConfig()
+    ->setMultiMsg(true)
+    ->setMaxMessageLength(2000) // at least 50 characters
+    ->setDatetimeFormat('Y/m/d H:i')
+    ->setTemplate("{datetime} {name}: {message}");
+
+// or you can create another Config instance and replace it:
+$otherConfig = new DiscordHandler\Config();
+$otherConfig->setWebHooks(['Other Hook Url 1', 'Other Hook Url 2']);
+
+$handler->setConfig($otherConfig);
+
+$log->pushHandler($handler);
 
 ```
--- src/DiscordHandler.php
+++ src/DiscordHandler.php
@@ -1,51 +1,118 @@
 <?php
+
 namespace DiscordHandler;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 use \Monolog\Logger;
 use \Monolog\Handler\AbstractProcessingHandler;
 
 class DiscordHandler extends AbstractProcessingHandler
 {
-	private $initialized = false;
-	private $guzzle;
+    /** @var Config */
+    protected $config;
 
-	private $name;
-	private $subname;
+    /**
+     * DiscordHandler constructor.
+     *
+     * @param array|string $webHooks
+     * @param string $name
+     * @param string $subName
+     * @param int $level
+     * @param bool $bubble
+     */
+    public function __construct($webHooks, $name, $subName = '', $level = Logger::DEBUG, $bubble = true)
+    {
+        parent::__construct($level, $bubble);
 
-	private $webhooks;
-	private $statement;
+        $this->config = (new Config())
+            ->setClient(new Client())
+            ->setName($name)
+            ->setSubName($subName)
+            ->setWebHooks($webHooks);
+    }
 
-	/**
-	 * MonologDiscordHandler constructor.
-	 * @param \GuzzleHttp\Client $guzzle
-	 * @param array $webhooks
-	 * @param int $level
-	 * @param bool $bubble
-	 */
-	public function __construct($webhooks, $name, $subname = '', $level = Logger::DEBUG, $bubble = true)
-	{
-		$this->name = $name;
-		$this->subname = $subname;
-		$this->guzzle = new \GuzzleHttp\Client();
-		$this->webhooks = $webhooks;
-		parent::__construct($level, $bubble);
-	}
+    /**
+     * @return Config
+     */
+    public function getConfig()
+    {
+        return $this->config;
+    }
 
-	/**
-	 * @param array $record
-	 */
-	protected function write(array $record)
-	{
-		$content = '[' . $record['datetime']->format('Y-m-d H:i:s') . '] ' . $this->name . '.' . $this->subname . '.' . $record['level_name'] . ': ' . $record['message'];
+    /**
+     * @param Config $config
+     *
+     * @return DiscordHandler
+     */
+    public function setConfig($config)
+    {
+        $this->config = $config;
 
-		$i = 0;
-		while ($i < count($this->webhooks)){
-			$req = $this->guzzle->request('POST', $this->webhooks[$i], [
-				'form_params' => [
-					'content' => $content
-				]
-			]);
-			$i++;
-		}
-	}
+        return $this;
+    }
+
+    /**
+     * @param array $record
+     *
+     * @throws GuzzleException
+     */
+    protected function write(array $record)
+    {
+        $content = strtr(
+            $this->config->getTemplate(),
+            [
+                '{datetime}' => $record['datetime']->format($this->config->getDatetimeFormat()),
+                '{name}' => $this->config->getName(),
+                '{subName}' => $this->config->getSubName(),
+                '{levelName}' => $record['level_name'],
+                '{message}' => $record['message'],
+            ]
+        );
+
+        $parts = $this->splitMessage($content);
+        foreach ($this->config->getWebHooks() as $webHook) {
+            foreach ($parts as $part) {
+                $this->send($webHook, $part);
+            }
+        }
+    }
+
+    /**
+     * @param string $content
+     *
+     * @return string[]
+     */
+    protected function splitMessage($content)
+    {
+        $maxMessageLength = $this->config->getMaxMessageLength();
+
+        if (!$maxMessageLength) {
+            return [$content];
+        }
+
+        if ($this->config->isMultiMsg()) {
+            return str_split($content, $maxMessageLength);
+        }
+
+        if (strlen($content) > $maxMessageLength) {
+            $truncated = strlen($content) - $maxMessageLength + 30;
+            $result = substr($content, 0, $maxMessageLength - 30) . " [...truncated $truncated characters]";
+
+            return [$result];
+        }
+
+        return [$content];
+    }
+
+    /**
+     * @param string $webHook
+     * @param string $content
+     *
+     * @throws GuzzleException
+     */
+    protected function send($webHook, $content)
+    {
+        $this->config->getClient()->request('POST', $webHook, ['form_params' => ['content' => $content]]);
+    }
 }
